e444497400be3973e3850b2d6444eac0819f4b74
Your Name <lafayette.seller@gmail.com>
2012-04-22 06:16:05 -0500

    End of ch 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,8 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
   
   before_save { |user| user.email = email.downcase }
 
@@ -19,5 +20,8 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, format: { with: VALID_EMAIL_REGEX },
   			uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
 
 end
